dd0faba183405178059acdffd74eba00e31f85de
ritabc <ritabc@gmail.com>
2018-09-28 16:39:40 -0700

    Continue work on heartbeat contra moves and positions seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 ## Note only improper and becket dances are handled
-
+### Question: should the %w be %symbol instead?
 ## For hard coding purposes, I will enter data like: %w(neighbor_swing) and the result will be ['neigbor_swing']. This will have to be converted to 'Neighbor Swing' on the Frontend
 
 ## Positions still need short lines down the hall positions, probably others too
@@ -7,6 +7,7 @@
 class Seed
   def self.begin
     seed = Seed.new
+    seed.populate_all_positions
     seed.populate_heartbeat_contra
   end
 
@@ -16,8 +17,17 @@ class Seed
       improper_progressed
       becket
       opposite_becket
-      side_of_set_with neighbor_ones_facing_up
-      side_of_set_with neighbor_ones_facing_down
+      side_of_set_with_neighbor_ones_facing_up
+      side_of_set_with_neighbor_ones_facing_down
+
+      long_lines_larks_facing_in_partner_on_same_side_ones_on_right
+      long_lines_larks_facing_in_partner_on_same_side_ones_on_left
+      long_lines_larks_facing_in_neighbor_on_same_side_ones_facing_down
+      long_lines_larks_facing_in_neighbor_on_same_side_ones_facing_up
+      long_lines_ravens_facing_in_partner_on_same_side_ones_on_right
+      long_lines_ravens_facing_in_partner_on_same_side_ones_on_left
+      long_lines_ravens_facing_in_neighbor_on_same_side_ones_facing_down
+      long_lines_ravens_facing_in_neighbor_on_same_side_ones_facing_up
 
       long_lines_larks_facing_in_partner_on_same_side_ones_on_right
       long_lines_larks_facing_in_partner_on_same_side_ones_on_left
@@ -36,7 +46,6 @@ class Seed
       short_wavy_lines_larks_in_middle_ones_on_left
       short_wavy_lines_larks_in_middle_ones_facing_down
       short_wavy_lines_larks_in_middle_ones_facing_up
-
       short_wavy_lines_r_orient_ones_in_middle_raven_one_facing_down
       short_wavy_lines_r_orient_ones_in_middle_raven_one_facing_up
       short_wavy_lines_r_orient_ones_on_outside_raven_one_facing_up
@@ -45,7 +54,6 @@ class Seed
       short_wavy_lines_r_orient_ones_on_left_raven_one_facing_down
       short_wavy_lines_r_orient_ones_on_right_raven_one_facing_down
       short_wavy_lines_r_orient_ones_on_right_raven_one_facing_down
-
       short_wavy_lines_s_orient_ones_in_middle_raven_one_facing_down
       short_wavy_lines_s_orient_ones_in_middle_raven_one_facing_up
       short_wavy_lines_s_orient_ones_on_outside_raven_one_facing_up
@@ -55,33 +63,50 @@ class Seed
       short_wavy_lines_s_orient_ones_on_right_raven_one_facing_down
       short_wavy_lines_s_orient_ones_on_right_raven_one_facing_down
     )
+    positions.each do |position_description|
+      Position.create!(description: position_description)
+    end
   end
 
-  def populate_heartbeat_contra
+  def populate_moves_for_heartbeat_contra
     dance = Dance.create!(name: 'Heartbeat Contra', writer: 'Don Flaherty', is_becket: false)
     moves = %w(balance_the_ring
       petronella
-      neigbor_swing
-      ravens_right_shoulder_round_1.5
+      neighbor_swing
+      dancers_on_left_right_shoulder_round_one_and_a_half
       partner_swing
-      circle_left_3/4
+      circle_left_three_quarters
       partner_balance
       california_twirl
     )
 
-    ## Which positions do I need to hard code now? All.
-    ## Which possible_move_start_positions do I need to hard code now? for each move, all possible_move_start_positions
-
-
-
     moves.each_with_index do |move_name, index|
       move = Move.create!(name: move_name)
+
       dance_move = DanceMove.create!(
         dance_id: dance.id,
         move_id: move.id,
-        number_in_dance: index,
-        ending_position_id:
+        number_in_dance: index
       )
+    end
+
+    def populate_possible_move_start_positions
+      ## for heartbeat contra moves
+      moves_and_possible_start_positions = {
+        'balance_the_ring' => %w(improper improper_progressed becket opposite_becket side_of_set_with_neighbor_ones_facing_up side_of_set_with_neighbor_ones_facing_down),
+        'petronella' => %w(improper improper_progressed becket opposite_becket side_of_set_with_neighbor_ones_facing_up side_of_set_with_neighbor_ones_facing_down),
+        'neighbor_swing' => %w(improper improper_progressed side_of_set_with_neighbor_ones_facing_up, side_of_set_with_neighbor_ones_facing_down)
+        'dancers_on_right_left_shoulder_round_one_and_a_half' => %w(improper improper_progressed becket opposite_becket side_of_set_with_neighbor_ones_facing_up side_of_set_with_neighbor_ones_facing_down )
+      }
+      moves_and_possible_start_positions.each do |move, possible_positions|
+        possible_positions.each do |position|
+          PossibleMoveStartPosition.create!(position_id: position.id, move_id: move.id)
+        end
+      end
+
+    end
+
+    def populate_ending_positions_for_heartbeat_contra
     end
 
 end
